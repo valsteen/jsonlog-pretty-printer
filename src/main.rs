16a47361fe86b668f287dfd864631f585e0daf6b
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-09-01 18:16:31 +0200

    simplify padding generator

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,30 @@ use std::iter::repeat;
 
 fn dive(indent: usize, value: Value) -> String {
     let left_padding = " ".repeat(indent);
-    let mut left_padding_generator = [""].into_iter().chain(repeat(&*left_padding));
+    let left_padding_generator = [""].into_iter().chain(repeat(&*left_padding));
 
     match value {
         Value::Null => "(null)".to_string(),
         Value::Bool(_) | Value::Number(_) => value.to_string(),
         Value::String(s) => s
             .split('\n')
-            .map(move |line| format!("{}{}", left_padding_generator.next().unwrap(), line))
+            .zip(left_padding_generator)
+            .map(|(line, padding)| format!("{}{}", padding, line))
             .join("\n"),
         Value::Array(a) => a
             .into_iter()
-            .map(|line| {
-                format!(
-                    "{}- {}",
-                    left_padding_generator.next().unwrap(),
-                    dive(indent + 2, line)
-                )
-            })
+            .zip(left_padding_generator)
+            .map(|(line, padding)| format!("{}- {}", padding, dive(indent + 2, line)))
             .join("\n"),
         Value::Object(o) => {
             let max_indent = o.keys().map(String::len).max().unwrap_or_default();
             o.into_iter()
-                .map(|(k, v)| {
+                .zip(left_padding_generator)
+                .map(|((k, v), padding)| {
                     let afterkey = " ".repeat(max_indent - k.len());
                     format!(
                         "{}{}{}: {}",
-                        left_padding_generator.next().unwrap(),
+                        padding,
                         k,
                         afterkey,
                         dive(indent + max_indent + 2, v)
