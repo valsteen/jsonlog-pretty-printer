66594dd704f19fad14d64e1bee3bd71ca55facbf
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-09-01 15:33:06 +0200

    reformat + reuse indentation

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-use std::io;
-use std::io::BufRead;
 use itertools::Itertools;
 use serde_json::Value;
-
+use std::io;
+use std::io::BufRead;
+use std::iter::repeat;
 
 fn dive(indent: usize, value: Value) -> String {
+    let left_padding = " ".repeat(indent);
+    let mut left_padding_generator = [""].into_iter().chain(repeat(&*left_padding));
+
     match value {
         Value::Null => "(null)".to_string(),
         Value::Bool(_) | Value::Number(_) => value.to_string(),
-        Value::String(s) => {
-            s.split("\n").enumerate().map(|(n, line)| {
-                let spaces = (n > 0).then(|| " ".repeat(indent)).unwrap_or_default();
-                format!("{}{}", spaces, line)
-            }).join("\n")
-        }
-        Value::Array(a) => {
-            a.into_iter().enumerate().map(|(n, line)| {
-                let spaces = (n > 0).then(|| " ".repeat(indent)).unwrap_or_default();
-                format!("{}- {}", spaces, dive(indent + 2, line))
-            }).join("\n")
-        }
+        Value::String(s) => s
+            .split('\n')
+            .map(move |line| format!("{}{}", left_padding_generator.next().unwrap(), line))
+            .join("\n"),
+        Value::Array(a) => a
+            .into_iter()
+            .map(|line| {
+                format!(
+                    "{}- {}",
+                    left_padding_generator.next().unwrap(),
+                    dive(indent + 2, line)
+                )
+            })
+            .join("\n"),
         Value::Object(o) => {
             let max_indent = o.keys().map(String::len).max().unwrap_or_default();
-            o.into_iter().enumerate().map(|(n, (k, v))| {
-                let beforekey = (n > 0).then(|| " ".repeat(indent)).unwrap_or_default();
-                let afterkey = " ".repeat(max_indent - k.len());
-                format!("{}{}{}: {}", beforekey, k, afterkey, dive(indent + max_indent + 2, v))
-            }).join("\n")
+            o.into_iter()
+                .map(|(k, v)| {
+                    let afterkey = " ".repeat(max_indent - k.len());
+                    format!(
+                        "{}{}{}: {}",
+                        left_padding_generator.next().unwrap(),
+                        k,
+                        afterkey,
+                        dive(indent + max_indent + 2, v)
+                    )
+                })
+                .join("\n")
         }
     }
 }
