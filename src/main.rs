f7f87072c2b93ca894651e46a55c336c57d7ea5c
Vincent Alsteen <vincent.alsteen@deliveryhero.com>
2022-09-02 13:36:50 +0200

    add some magic to try to catch JSON shoved in raw strings

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,25 @@ fn dive(indent: usize, value: Value) -> String {
         Value::Null => "(null)".to_string(),
         Value::Bool(_) | Value::Number(_) => value.to_string(),
         Value::String(s) => {
-            if let Ok(parsed) = serde_json::from_str::<Value>(&*s) {
-                dive(indent, parsed)
-            } else {
-                s.split('\n')
-                    .zip(left_padding_generator)
-                    .map(|(line, padding)| format!("{}{}", padding, line))
-                    .join("\n")
+            // try hard to parse JSON strings shoved in regular strings
+            // looking at you dd-trace-go 👀
+            for (position, c) in s.chars().enumerate() {
+                if let Some(Ok(value)) =
+                    ("{\"[".contains(c)).then(|| serde_json::from_str::<Value>(&s[position..]))
+                {
+                    let dive_into = if position == 0 {
+                        value
+                    } else {
+                        Value::Array(vec![Value::String(s[..position].to_string()), value])
+                    };
+                    return dive(indent, dive_into);
+                }
             }
+
+            s.split('\n')
+                .zip(left_padding_generator)
+                .map(|(line, padding)| format!("{}{}", padding, line))
+                .join("\n")
         }
         Value::Array(a) => a
             .into_iter()
